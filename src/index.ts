3f595123959f4559705a6130eeee323824d5a362
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-02 16:27:05 +0300

    addEvents() and other enhancements

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import { Board } from "./ts/Board";
 
+const { ipcRenderer } = require('electron')
+
 let canvas;
 let context;
-//let board : Board;
+let board : Board;
 
 function init() {
-    canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    canvas = <HTMLCanvasElement> document.querySelector("#canvas");
     context = <CanvasRenderingContext2D> canvas.getContext("2d");
 
-    //board = new Board(12, 8);
+    board = new Board(12, 8);
 
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
+
+    addEvents();
 }
 
 window.onload = init;
+
+function saveFile() {
+    console.log("save file to disk");
+}
+
+function addEvents() {
+    // https://stackoverflow.com/questions/43102216/button-click-event-binding-in-electron-js
+    const saveButton = <HTMLElement> document.querySelector('#save-btn')
+    saveButton.addEventListener('click', () => {
+        saveFile();
+    });
+}
 
 // https://www.codegrepper.com/code-examples/javascript/typescript+get+mouse+location+from+event
 // https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
